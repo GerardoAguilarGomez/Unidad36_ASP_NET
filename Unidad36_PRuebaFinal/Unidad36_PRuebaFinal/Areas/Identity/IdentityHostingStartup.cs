bbe7025da20bb71ac91764db7f86c319e35e8af0
GerardoAguilarGomez <gerardo.aguilar@grupocastilla.es>
2021-03-01 23:38:48 +0100

    Unidad 36

---
+++ Unidad36_PRuebaFinal/Unidad36_PRuebaFinal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Unidad36_PRuebaFinal.Data;
+
+[assembly: HostingStartup(typeof(Unidad36_PRuebaFinal.Areas.Identity.IdentityHostingStartup))]
+namespace Unidad36_PRuebaFinal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Unidad36_PRuebaFinal/Unidad36_PRuebaFinal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Unidad36_PRuebaFinal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
